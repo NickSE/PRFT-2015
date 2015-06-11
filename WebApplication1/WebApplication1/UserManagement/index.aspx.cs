c29555a583bbdd25806d9b34c723902dc5ff4a3e
Pigna <Myron.antonissen@gmail.com>
2015-06-11 14:35:06 +0200

    Add Usermanagement interface

---
+++ WebApplication1/WebApplication1/UserManagement/index.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using WebApplication1.DB;
+
+namespace WebApplication1.UserManagement
+{
+    public partial class index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void btnCreateUser_Click(object sender, EventArgs e)
+        {
+
+        }
+        protected void btnRemoveUser_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
